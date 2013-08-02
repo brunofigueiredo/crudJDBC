1c5d5b98645edce651f82a38ef88d7b7bf4c8f8e
bruno_figueiredo <bruno_figueiredo@10.1.48.191>
2013-08-02 15:30:04 -0300

    Classe modelo e todos os seus dados

---
+++ crudJDBC/src/br/com/modelo/negocio/Modelo.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.modelo.negocio;
+
+/**
+ *
+ * @author bruno_figueiredo
+ */
+public class Modelo {
+    private int id;
+    private String descricao;
+    private int potencia;
+    private Marca marca;
+    
+    
+    
+    public Modelo(){
+        
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getPotencia() {
+        return potencia;
+    }
+
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;
+    }
+
+    public Marca getMarca() {
+        return marca;
+    }
+
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+}
